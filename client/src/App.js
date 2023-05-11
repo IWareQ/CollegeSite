465772a5bcd639fb143740b2db4e2b6a21816052
IWareQ <dimonpo96@mail.ru>
2023-05-11 21:18:33 +0400

    Code style

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,24 @@ import './styles/App.css';
 import './styles/button.css';
 
 function App() {
-    return (
-        <div className="container">
-            <header className="header">
-                <div className="logo">CollegeSite</div>
-                <nav className="links">
-                    <a href="#">Главная</a>
-                    <a href="#">О колледже</a>
-                    <a href="#">Абитуриентам</a>
-                    <a href="#">Студентам</a>
-                </nav>
-                <a href="#" className="shine-button">
-                    <b>Вход</b>
-                </a>
-            </header>
-        </div>
-    );
+	return (
+		<div>
+			<div className="container">
+				<header className="header">
+					<div className="logo">CollegeSite</div>
+					<nav className="links">
+						<a href="#">Главная</a>
+						<a href="#">О колледже</a>
+						<a href="#">Абитуриентам</a>
+						<a href="#">Студентам</a>
+					</nav>
+					<a href="#" className="shine-button">
+						<b>Вход</b>
+					</a>
+				</header>
+			</div>
+		</div>
+	);
 }
 
 export default App;
