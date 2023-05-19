68a1b682942da2fa8351945988744a33dbddb919
v1adik123 <Vladik1122333@gmail.com>
2023-05-19 21:16:08 +0300

    Add Typewriter component, minor bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,26 @@ import React from 'react';
 import './styles/App.css';
 import './styles/button.css';
 import {ReactComponent as Man} from './Man.svg';
+import Typewriter from './components/Typewriter';
+import './styles/typewriter.css';
+
+
+
 
 function App() {
 	return (
 		<div>
 			<div className="container">
 				<header className="header">
-					<div className="logo">Колледж <span className=""><br/>Связи</span></div>
+					<div className="logo">Колледж <span className="logo-msg2"><br/>Связи</span></div>
 
 					<nav className="links">
-						<a href="#">О колледже</a>
-						<a href="#">Абитуриентам</a>
-						<a href="#">Студентам</a>
+						<a className="links" href="#">О колледже</a>
+						<a className="links" href="#">Абитуриентам</a>
+						<a className="links" href="#">Студентам</a>
 					</nav>
 					<div className="phone-site">
-						<a href="tel:+7 (846) 932 25 35">+7 (846) 932 25 35</a>
+						<a className="phone-site" href="tel:+7 (846) 932 25 35">+7 (846) 932 25 35</a>
 					</div>
 					<a href="#" className="login-button">
 						<b>Вход</b>
@@ -24,16 +29,18 @@ function App() {
 				</header>
 			</div>
 			<div className="college-msg">
-				<div className="msg1">Колледж, <br/>
-					<div className="msg2">где учат профессиям будущего</div>
-				</div>
-			</div>
-			<div className="enroll">
+				<div className="text-wrapper">
+					<Typewriter/>
+				<div className="msg1">Колледж, <br/></div>
+				<div className="msg2"><span id="typewriter"></span><span id="cursor">|</span></div>
 				<a href="" className="enroll-button">Поступить</a>
+				</div>
+					<div className="image">
+						<Man/>
+					</div>
 			</div>
-			<div className="image-man">
-				<Man/>
-			</div>
+
+
 		</div>
 	);
 }
---
+++ client/src/components/Typewriter.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+function Typewriter(){
+	function sleep(ms) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+
+	const phrases = ["где учат профессиям будущего", "в который поступают лидеры", "студентам которого завидуют"];
+	const el = document.getElementById("typewriter");
+
+	let sleepTime = 100;
+
+	let curPhraseIndex = 0;
+
+	const writeLoop = async () => {
+		while (true) {
+			let curWord = phrases[curPhraseIndex];
+
+			for (let i = 0; i < curWord.length; i++) {
+				el.innerText = curWord.substring(0, i + 1);
+				await sleep(sleepTime);
+			}
+
+			await sleep(sleepTime * 10);
+
+			for (let i = curWord.length; i > 0; i--) {
+				el.innerText = curWord.substring(0, i - 1);
+				await sleep(sleepTime);
+			}
+
+			await sleep(sleepTime * 5);
+
+			if (curPhraseIndex === phrases.length - 1) {
+				curPhraseIndex = 0;
+			} else {
+				curPhraseIndex++;
+			}
+		}
+	};
+
+	writeLoop();
+}
+
+export default Typewriter;
